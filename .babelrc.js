6a0cf1c0c79b1886eb5816fb618fe2c88aaa72e5
Paul Chong <paulhyunchong@gmail.com>
2020-03-28 14:24:24 -0700

    undo gh-pages changes

--- .babelrc.js
+++
@@ -1,9 +0,0 @@
-const env = require('./env-config');
-console.log(env);
-module.exports = {
-  presets: ['next/babel'],
-  plugins: [
-    ['styled-components', { ssr: true }],
-    ['transform-define', env]
-  ]
-};
--- next.config.js
+++ next.config.js
@@ -1,9 +1,2 @@
 const withImages = require('next-images');
-const debug = process.env.NODE_ENV !== 'production';
-
-module.exports = withImages({
-  assetPrefix: !debug ? '/nextjs-c4/' : '',
-  webpack(config, options) {
-    return config;
-  }
-});
+module.exports = withImages();
