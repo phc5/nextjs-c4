a99c3d3f299e02750bdad2ad7ad2664b0f966044
Paul Chong <paulhyunchong@gmail.com>
2020-03-28 14:00:16 -0700

    Adds project name to config files

--- next.config.js
+++ next.config.js
@@ -3,5 +3,5 @@ const withImages = require('next-images');
 const debug = process.env.NODE_ENV !== 'production';
 
 module.exports = withImages({
-  assetPrefix: !debug ? '/Next-gh-page-example/' : ''
+  assetPrefix: !debug ? '/nextjs-c4/' : ''
 });
