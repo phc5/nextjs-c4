4cfd8a31036a7d289ca84b1fc63cf3501b16508f
Paul Chong <paulhyunchong@gmail.com>
2020-03-28 14:09:44 -0700

    Remove assetPreix in config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,2 @@
 const withImages = require('next-images');
-
-const debug = process.env.NODE_ENV !== 'production';
-
-module.exports = withImages({
-  assetPrefix: !debug ? '/nextjs-c4/' : ''
-});
+module.exports = withImages();
