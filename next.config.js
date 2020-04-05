8f41024b82c5feab01550cf698546b4f42627c37
Paul Chong <paulhyunchong@gmail.com>
2020-04-05 10:55:12 -0700

    Moves api key to .env file and uses new key

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
+require('dotenv').config();
 const withImages = require('next-images');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 });
 
-module.exports = withBundleAnalyzer(withImages(
-  {
+module.exports = withBundleAnalyzer(
+  withImages({
     env: {
-      youtubeApiKey: 'AIzaSyCGxeq_EDpz22jTACuCXE7G5Il48B1IPyk', //get from console.developers.google.com using c4ministrywebmaster account
-      youtubeChannelId: 'UCRKb2u0ePuQ9t6TnKVfe1Gw'
+      youtubeApiKey: process.env.YOUTUBE_API_KEY,
+      youtubeChannelId: process.env.YOUTUBE_CHANNEL_ID
     }
-  }
-));
+  })
+);
