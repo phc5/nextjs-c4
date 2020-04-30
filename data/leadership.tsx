36712a78421995f49ed02dcaf0c6f59ac5a2aa8a
Paul Chong <paulhyunchong@gmail.com>
2020-04-30 13:51:50 -0700

    Updates placeholder data

--- data/leadership.tsx
+++ data/leadership.tsx
@@ -4,30 +4,30 @@ export default [
     name: 'Daniel Park',
     info:
       'Pastor Daniel graudated from Calvin College in 2006 for his BA and from Westminster Theological Semintary (PA) in 2010 with an MDiv. He was ordained in the PCA on 2013. His passions include spending time with his wife and four dogs and spending time with his church members until the break of dawn.',
-    imagePath: '/assets/portraits/dp.jpg'
+    imagePath: '/assets/portraits/dp.jpg',
   },
   {
     title: 'Hospitality Chair',
     name: 'Ellen Lee',
-    info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quis lorem ac nulla volutpat consequat. Maecenas dictum nunc sed lacus venenatis, vitae condimentum augue vulputate. Praesent at vestibulum purus, eu pulvinar nibh.',
-    imagePath: '/assets/portraits/ellen.jpg'
+    info: '',
+    imagePath: '/assets/portraits/ellen.jpg',
   },
   {
     title: 'Treasury & Communications Chair',
     name: 'Paul Chong',
-    info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quis lorem ac nulla volutpat consequat. Maecenas dictum nunc sed lacus venenatis, vitae condimentum augue vulputate. Praesent at vestibulum purus, eu pulvinar nibh.',
-    imagePath: '/assets/portraits/noimage.png'
+    info: '',
+    imagePath: '/assets/portraits/noimage.png',
   },
   {
     title: 'Praise Leader',
     name: 'Daniel Yoon',
-    info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quis lorem ac nulla volutpat consequat. Maecenas dictum nunc sed lacus venenatis, vitae condimentum augue vulputate. Praesent at vestibulum purus, eu pulvinar nibh.',
-    imagePath: '/assets/portraits/noimage.png'
+    info: '',
+    imagePath: '/assets/portraits/noimage.png',
   },
   {
     title: 'Staff',
     name: 'Rebekah Park',
-    info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quis lorem ac nulla volutpat consequat. Maecenas dictum nunc sed lacus venenatis, vitae condimentum augue vulputate. Praesent at vestibulum purus, eu pulvinar nibh.',
-    imagePath: '/assets/portraits/rebekah.jpg'
+    info: '',
+    imagePath: '/assets/portraits/rebekah.jpg',
   },
 ];
